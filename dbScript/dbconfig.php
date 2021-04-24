920370c341fd718a72185db028de86e141c16b02
NaxNax666 <NaxNax@MSI-ANDREW>
2021-04-24 23:12:35 +0300

    server

--- dbScript/dbconfig.php
+++ dbScript/dbconfig.php
@@ -1,6 +1,6 @@
 <?php
-$db_name = "kurs";
-$db_adress = "localhost";
-$db_login  = "root";
-$db_password = "";
+$db_name = "heroku_d6539fac3d53199";
+$db_adress = "us-cdbr-east-03.cleardb.com";
+$db_login  = "b60afedce59482";
+$db_password = "78a9faed";
 ?>
