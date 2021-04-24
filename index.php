9a77f814586891526808645948705d8e1ccff524
NaxNax666 <NaxNax@MSI-ANDREW>
2021-04-25 00:10:28 +0300

    server

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 require __DIR__ . '/authcore/auth.php';
-$login = getUserLogin();
-if ($login === null):
-    header('Location: authcore/login.php');
-else:
+//$login = getUserLogin();
+//if ($login === null):
+    //header('Location: authcore/login.php');
+//else:
     header('Location: main.php');
-endif; ?>
+//endif; ?>
